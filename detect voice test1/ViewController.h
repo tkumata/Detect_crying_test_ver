865f7e945f416eca871601dc9e9427d610b60087
tkumata <tkumata@gmail.com>
2015-10-21 16:07:32 +0900

    Commit init.

--- detect voice test1/ViewController.h
+++ detect voice test1/ViewController.h
@@ -7,9 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface ViewController : UIViewController
+{
+    AudioQueueRef   _queue;     // 音声入力用のキュー
+    NSTimer         *_timer;    // 監視タイマー
+}
 
+@property (weak, nonatomic) IBOutlet UILabel *loudLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField *peakTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *averageTextField;
 
 @end
 
